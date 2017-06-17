4b62332fb449265a692371760fa086033bdd0a3b
shinichi kogai <kogai0121@gmail.com>
2017-06-17 10:58:55 +0900

    Add spec for unintended format of indent

---
+++ tests/source/file-lines-7.rs
@@ -0,0 +1,7 @@
+// rustfmt-file_lines: [{"file":"tests/source/file-lines-7.rs","range":[4,5]}]
+
+      pub fn   main()
+{
+let world1 = "world1"; println!("Hello-1, {}!", world1);
+      let   world2     = "world2"; println!("Hello-2, {}!", world2);
+       }
